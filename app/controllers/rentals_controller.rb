c18af882b62b6ae9cad62310045d04d8f4973187
Preston Davies <pdavies88@gmail.com>
2015-08-11 13:54:25 -0400

    working on backend functionality

--- app/controllers/rentals_controller.rb
+++ app/controllers/rentals_controller.rb
@@ -2,15 +2,29 @@ class RentalsController < ApplicationController
   before_action :authenticate_user!
 
 def index
+  @user = User.find(current_user)
+  @rental = Rental.all
 end
 
 def show
+  @user = User.find_by(session[:user_id])
+  @rental = Rental.all
 end
 
 def new
+  @user = User.find_by(session[:user_id])
+  @rental = Rental.new
 end
 
 def create
+    @user = User.find_by(session[:user_id])
+    @rental = @user.rentals.create!(rental_params)
+    @rental.user = current_user
+    if @rental.save
+      redirect_to user_rentals_path(@user)
+    else
+      redirect_to new_user_rental_path
+    end
 end
 
 def edit
@@ -20,6 +34,11 @@ def update
 end
 
 def destroy
+end
+
+private
+def rental_params
+  params.require(:rental).permit(:name, :staff, :email, :phone, :item)
 end
 
 end
--- db/migrate/20150810172527_create_rentals.rb
+++ db/migrate/20150810172527_create_rentals.rb
@@ -2,8 +2,10 @@ class CreateRentals < ActiveRecord::Migration
   def change
     create_table :rentals do |t|
       t.string :name
-      t.string :type
-      t.string :request
+      t.string :staff 
+      t.string :email
+      t.string :phone
+      t.string :item
       t.datetime "created_at", null: false
       t.datetime "updated_at", null: false
       t.integer :user_id
